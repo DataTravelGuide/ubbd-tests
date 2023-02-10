8760c8696fb35c73e7838999329052db7ce01bfa
Dongsheng Yang <dongsheng.yang.linux@gmail.com>
2023-02-10 10:38:10 +0800

    introduce quick testing
    Signed-off-by: Dongsheng Yang <dongsheng.yang.linux@gmail.com>

---
+++ all_test_quick.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+
+from avocado.core.job import Job
+from avocado.core.suite import TestSuite
+
+ubbdadmtest_config = {'resolver.references': ['ubbdadmtest.py:Ubbdadmtest.test'],
+          'yaml_to_mux.files': ['ubbdadmtest.py.data/ubbdadmtest_quick.yaml'],
+          'nrunner.max_parallel_tasks': 1,
+          'run.dry_run.enabled': False}
+
+xfstests_config = {'resolver.references': ['xfstests.py:Xfstests.test'],
+          'yaml_to_mux.files': ['xfstests.py.data/xfstests_quick.yaml'],
+          'nrunner.max_parallel_tasks': 1,
+          'run.dry_run.enabled': False}
+
+fio_config = {'resolver.references': ['fio.py:Fiotest.test'],
+          'yaml_to_mux.files': ['fio.py.data/fio_quick.yaml'],
+          'nrunner.max_parallel_tasks': 1,
+          'run.dry_run.enabled': False}
+
+upgradeonline_config = {'resolver.references': ['upgradeonline.py:Upgradeonlinetest.test'],
+          'yaml_to_mux.files': ['upgradeonline.py.data/upgradeonline_quick.yaml'],
+          'nrunner.max_parallel_tasks': 1,
+          'run.dry_run.enabled': False}
+
+test_suites = []
+
+test_suites.append(TestSuite.from_config(upgradeonline_config, name='upgradeonline'))
+test_suites.append(TestSuite.from_config(ubbdadmtest_config, name='ubbdadmtest'))
+test_suites.append(TestSuite.from_config(xfstests_config, name='xfstests'))
+test_suites.append(TestSuite.from_config(fio_config, name='fio'))
+
+with Job(test_suites=test_suites) as j:
+    sys.exit(j.run())
--- test_all.sh
+++ test_all.sh
@@ -2,6 +2,14 @@
 
 date_str=`date "+%Y_%m_%d_%H_%M_%S"`
 
+SUFFIX=""
+if [ "$1" = "quick" ]; then
+	SUFFIX="_quick"
+	echo "quick ubbd test."
+else
+	echo "full ubbd test."
+fi
+
 cat ./local_conf
 . ./local_conf
 . ./utils/utils.sh
@@ -31,23 +39,23 @@ setup
 prepare_ubbdd 0
 
 cd ${UBBD_TESTS_DIR}
-replace_option cachebackendtest.py.data/cachebackendtest.yaml UBBD_DIR_DEFAULT ${UBBD_DIR}
-replace_option cachebackendtest.py.data/cachebackendtest.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
-replace_option cachebackendtest.py.data/cachebackendtest.yaml UBBD_DEV_SIZE_DEFAULT 1048576000
-replace_option cachebackendtest.py.data/cachebackendtest.yaml UBBD_CACHE_FILE_DEFAULT "/dev/ram0p1"
-replace_option cachebackendtest.py.data/cachebackendtest.yaml UBBD_CACHE_FILE_SIZE_DEFAULT 1048576000
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml UBBD_DIR_DEFAULT ${UBBD_DIR}
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml UBBD_DEV_SIZE_DEFAULT 1048576000
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml UBBD_CACHE_FILE_DEFAULT "/dev/ram0p1"
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml UBBD_CACHE_FILE_SIZE_DEFAULT 1048576000
 
-replace_option cachebackendtest.py.data/cachebackendtest.yaml UBBD_BACKING_FILE_DEFAULT "/dev/ram0p2"
-replace_option cachebackendtest.py.data/cachebackendtest.yaml UBBD_BACKING_FILE_SIZE_DEFAULT 1048576000
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml UBBD_BACKING_FILE_DEFAULT "/dev/ram0p2"
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml UBBD_BACKING_FILE_SIZE_DEFAULT 1048576000
 
-replace_option cachebackendtest.py.data/cachebackendtest.yaml S3_ACCESS_ID ${UBBD_S3_ACCESSID}
-replace_option cachebackendtest.py.data/cachebackendtest.yaml S3_ACCESS_KEY ${UBBD_S3_ACCESSKEY}
-replace_option cachebackendtest.py.data/cachebackendtest.yaml S3_HOSTNAME ${UBBD_S3_HOSTNAME}
-replace_option cachebackendtest.py.data/cachebackendtest.yaml S3_PORT ${UBBD_S3_PORT}
-replace_option cachebackendtest.py.data/cachebackendtest.yaml S3_BUCKET_NAME ${UBBD_BUCKET_NAME}
-replace_option cachebackendtest.py.data/cachebackendtest.yaml S3_DEV_SIZE_DEFAULT 31457280
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml S3_ACCESS_ID ${UBBD_S3_ACCESSID}
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml S3_ACCESS_KEY ${UBBD_S3_ACCESSKEY}
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml S3_HOSTNAME ${UBBD_S3_HOSTNAME}
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml S3_PORT ${UBBD_S3_PORT}
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml S3_BUCKET_NAME ${UBBD_BUCKET_NAME}
+replace_option cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml S3_DEV_SIZE_DEFAULT 31457280
 
-avocado run --nrunner-max-parallel-tasks 1  cachebackendtest.py -m cachebackendtest.py.data/cachebackendtest.yaml
+avocado run --nrunner-max-parallel-tasks 1  cachebackendtest.py -m cachebackendtest.py.data/cachebackendtest${SUFFIX}.yaml
 
 if [ ! -z "$UBBD_TESTS_POST_TEST_CMDS" ]; then
 	${UBBD_TESTS_POST_TEST_CMDS}
@@ -73,18 +81,18 @@ setup
 prepare_ubbdd 0
 
 cd ${UBBD_TESTS_DIR}
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml UBBD_DIR_DEFAULT ${UBBD_DIR}
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml UBBD_B_FILE_DEFAULT "/dev/ram0p1"
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml UBBD_B_FILE_SIZE_DEFAULT 1048576000
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml UBBD_DIR_DEFAULT ${UBBD_DIR}
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml UBBD_B_FILE_DEFAULT "/dev/ram0p1"
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml UBBD_B_FILE_SIZE_DEFAULT 1048576000
 
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml S3_ACCESS_ID ${UBBD_S3_ACCESSID}
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml S3_ACCESS_KEY ${UBBD_S3_ACCESSKEY}
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml S3_HOSTNAME ${UBBD_S3_HOSTNAME}
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml S3_PORT ${UBBD_S3_PORT}
-replace_option ubbdadmtest.py.data/ubbdadmtest.yaml S3_BUCKET_NAME ${UBBD_BUCKET_NAME}
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml S3_ACCESS_ID ${UBBD_S3_ACCESSID}
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml S3_ACCESS_KEY ${UBBD_S3_ACCESSKEY}
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml S3_HOSTNAME ${UBBD_S3_HOSTNAME}
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml S3_PORT ${UBBD_S3_PORT}
+replace_option ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml S3_BUCKET_NAME ${UBBD_BUCKET_NAME}
 
-avocado run --nrunner-max-parallel-tasks 1  ubbdadmtest.py -m ubbdadmtest.py.data/ubbdadmtest.yaml
+avocado run --nrunner-max-parallel-tasks 1  ubbdadmtest.py -m ubbdadmtest.py.data/ubbdadmtest${SUFFIX}.yaml
 
 if [ ! -z "$UBBD_TESTS_POST_TEST_CMDS" ]; then
 	${UBBD_TESTS_POST_TEST_CMDS}
@@ -114,33 +122,33 @@ prepare_ubbd_devs
 
 # replace default options with the real options
 cd ${UBBD_TESTS_DIR}
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml UBBD_DIR_DEFAULT ${UBBD_DIR}
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml UBBD_B_FILE_DEFAULT "/dev/ram0p1"
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml UBBD_B_FILE_SIZE_DEFAULT 1048576000
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml UBBD_DIR_DEFAULT ${UBBD_DIR}
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml UBBD_B_FILE_DEFAULT "/dev/ram0p1"
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml UBBD_B_FILE_SIZE_DEFAULT 1048576000
 
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml S3_ACCESS_ID ${UBBD_S3_ACCESSID}
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml S3_ACCESS_KEY ${UBBD_S3_ACCESSKEY}
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml S3_HOSTNAME ${UBBD_S3_HOSTNAME}
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml S3_PORT ${UBBD_S3_PORT}
-replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject.yaml S3_BUCKET_NAME ${UBBD_BUCKET_NAME}
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml S3_ACCESS_ID ${UBBD_S3_ACCESSID}
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml S3_ACCESS_KEY ${UBBD_S3_ACCESSKEY}
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml S3_HOSTNAME ${UBBD_S3_HOSTNAME}
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml S3_PORT ${UBBD_S3_PORT}
+replace_option ubbdadmtest.py.data/ubbdadmtest_fault_inject${SUFFIX}.yaml S3_BUCKET_NAME ${UBBD_BUCKET_NAME}
 
-replace_option xfstests.py.data/xfstests.yaml XFSTESTS_DIR_DEFAULT ${UBBD_TESTS_XFSTESTS_DIR}
-replace_option xfstests.py.data/xfstests.yaml UBBD_DIR_DEFAULT ${UBBD_DIR}
-replace_option xfstests.py.data/xfstests.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
-replace_option xfstests.py.data/xfstests.yaml SCRATCH_MNT_DEFAULT ${XFSTESTS_SCRATCH_MNT}
-replace_option xfstests.py.data/xfstests.yaml TEST_MNT_DEFAULT ${XFSTESTS_TEST_MNT}
-replace_option xfstests.py.data/xfstests.yaml TEST_DEV_DEFAULT /dev/ubbd0
-replace_option xfstests.py.data/xfstests.yaml SCRATCH_DEV_DEFAULT /dev/ubbd1
+replace_option xfstests.py.data/xfstests${SUFFIX}.yaml XFSTESTS_DIR_DEFAULT ${UBBD_TESTS_XFSTESTS_DIR}
+replace_option xfstests.py.data/xfstests${SUFFIX}.yaml UBBD_DIR_DEFAULT ${UBBD_DIR}
+replace_option xfstests.py.data/xfstests${SUFFIX}.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
+replace_option xfstests.py.data/xfstests${SUFFIX}.yaml SCRATCH_MNT_DEFAULT ${XFSTESTS_SCRATCH_MNT}
+replace_option xfstests.py.data/xfstests${SUFFIX}.yaml TEST_MNT_DEFAULT ${XFSTESTS_TEST_MNT}
+replace_option xfstests.py.data/xfstests${SUFFIX}.yaml TEST_DEV_DEFAULT /dev/ubbd0
+replace_option xfstests.py.data/xfstests${SUFFIX}.yaml SCRATCH_DEV_DEFAULT /dev/ubbd1
 
 
-replace_option fio.py.data/fio.yaml UBBD_DEV_PATH /dev/ubbd2
-replace_option fio.py.data/fio.yaml OUTPUT_FILE ${FIOTEST_OUTFILE}
+replace_option fio.py.data/fio${SUFFIX}.yaml UBBD_DEV_PATH /dev/ubbd2
+replace_option fio.py.data/fio${SUFFIX}.yaml OUTPUT_FILE ${FIOTEST_OUTFILE}
 
-replace_option upgradeonline.py.data/upgradeonline.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
-replace_option upgradeonline.py.data/upgradeonline.yaml UBBD_DEV_DEFAULT /dev/ubbd0
+replace_option upgradeonline.py.data/upgradeonline${SUFFIX}.yaml UBBD_TESTS_DIR_DEFAULT ${UBBD_TESTS_DIR}
+replace_option upgradeonline.py.data/upgradeonline${SUFFIX}.yaml UBBD_DEV_DEFAULT /dev/ubbd0
 
-./all_test.py
+./all_test${SUFFIX}.py
 
 if [ ! -z "$UBBD_TESTS_POST_TEST_CMDS" ]; then
 	${UBBD_TESTS_POST_TEST_CMDS}
