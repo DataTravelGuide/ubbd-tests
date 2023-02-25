46ddd071115a97b5ba86f70ebe96e48b9c68837b
Dongsheng Yang <dongsheng.yang.linux@gmail.com>
2023-02-25 13:08:04 +0000

    start_ubbdd_killer.sh: wait for ubbdd started before next killing
    Signed-off-by: Dongsheng Yang <dongsheng.yang.linux@gmail.com>

--- utils/start_ubbdd_killer.sh
+++ utils/start_ubbdd_killer.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+wait_for_ubbdd ()
+{
+	while true ; do
+		ubbdadm list
+		if [ $? -eq 0 ]; then
+			return
+		fi
+		sleep 1
+	done
+}
+
 sleep_time=$1
 
 while true; do
 	pkill ubbd-backend
 	pkill ubbdd
 	ps -ef|grep ubbdd|grep memleak|gawk '{print "kill "$2}'|bash
+
+	wait_for_ubbdd
 	sleep $(($RANDOM % ${sleep_time}))
 done
